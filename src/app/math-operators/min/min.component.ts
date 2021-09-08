83df5c7d6eb6583980ed732ffb21e653afe81a8b
WethinkGary <gary841116@gmail.com>
2021-09-08 12:03:00 +0800

    1 file modified

--- src/app/math-operators/min/min.component.ts
+++ src/app/math-operators/min/min.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, merge } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
+import { interval, merge, of } from 'rxjs';
+import { map, min, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-min',
@@ -13,5 +13,24 @@ export class MinComponent implements OnInit, OnDestroy {
     this.alive = false;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    /**
+     * 判斷來源Observable最小的值，作為訂閱的結果
+     */
+    of(5, 1, 9, 8)
+      .pipe(min())
+      .subscribe(data => {
+        console.log('min = ' + data);
+      });
+
+    of(5)
+      .pipe(
+        min((data1, data2) => {
+          return data1 - data2;
+        })
+      )
+      .subscribe(data => {
+        console.log('min = ' + data);
+      });
+  }
 }
